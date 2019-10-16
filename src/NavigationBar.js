678561407239a28e5d0ed06baea4c039dcc6f0c5
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-10-15 22:38:49 -0600

    Set up NavigationBar component

---
+++ src/NavigationBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './NavigationBar.scss';
+
+const NavigationBar = () => {
+  return (
+    <ul className='navigation-ul'>
+      <li>Home</li>
+      <li>Projects</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  )
+}
+
+export default NavigationBar;
