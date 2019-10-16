22e5cd89fe0f431b4e37b8eb22eb6bc5ee685439
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-10-16 00:53:52 -0600

    Delete punctuation mark from name

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <NavigationBar />
       <section className='title-section'>
         <p>Hello! ¡Holá! Bonjour!</p>
-        <h1>Edwin Del Bosque.</h1>
+        <h1>Edwin Del Bosque</h1>
         <h1 className='sub-header-title'>I build things for the web.</h1>
         <p className='description-title'>I'm a <span>front-end software developer</span> based in Denver, CO specializing in building (and occasionally designing) exceptional, high-quality websites and applications.</p>
         <a
