cd7d8028fae08e1ea36dfd77c6534dd9d83d127e
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-10-15 23:51:28 -0600

    Style main splash screen

--- src/App.js
+++ src/App.js
@@ -6,8 +6,14 @@ function App() {
   return (
     <main>
       <NavigationBar />
-      <h1>Edwin Del Bosque</h1>
-    </main>
+      <section className='title-section'>
+        <p>Hello, my name is</p>
+        <h1>Edwin Del Bosque.</h1>
+        <h1 className='sub-header-title'>I build things for the web.</h1>
+        <p className='description-title'>I'm a frontend software engineer based in Denver, CO specializing in building (and occasionally designing) exceptional, high-quality websites and applications.</p>
+        <a href='mailto:edwinbosq@gmail.com'><button className='contact-button'>Get In Touch</button></a>
+      </section>
+    </main >
   );
 }
 
