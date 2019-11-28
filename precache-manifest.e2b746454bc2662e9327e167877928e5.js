7a8d12a94ba1ad65e582ebb36fd37c5330ab2661
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-11-28 01:22:58 -0700

    Updates

---
+++ precache-manifest.e2b746454bc2662e9327e167877928e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f417e9b4a94eecbba41a5122cde45b84",
+    "url": "./index.html"
+  },
+  {
+    "revision": "362d5b4a654430151c5d",
+    "url": "./static/css/main.c422d6ce.chunk.css"
+  },
+  {
+    "revision": "0bb8e222b5a926c11957",
+    "url": "./static/js/2.4103014c.chunk.js"
+  },
+  {
+    "revision": "362d5b4a654430151c5d",
+    "url": "./static/js/main.608f483d.chunk.js"
+  },
+  {
+    "revision": "d9fec5f6efaf04749bab",
+    "url": "./static/js/runtime-main.c6822cd3.js"
+  }
+]);
