4357a1f9ae98142ba3b35a346e9b9cbe5b22f5eb
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-11-27 18:57:39 -0700

    Re-style splash screen

--- src/App.js
+++ src/App.js
@@ -3,25 +3,30 @@ import './App.scss';
 import NavigationBar from './NavigationBar';
 
 function App() {
-  return (
-    <main>
-      <NavigationBar />
-      <section className='title-section'>
-        <p>Hello! ¡Holá! Bonjour!</p>
-        <h1>Edwin Del Bosque</h1>
-        <h1 className='sub-header-title'>I build things for the web.</h1>
-        <p className='description-title'>I'm a <span>front-end software developer</span> based in Denver, CO specializing in building (and occasionally designing) exceptional, high-quality websites and applications.</p>
-        <a
-          href='mailto:edwinbosq@gmail.com'
-        ><button className='contact-button'>Get in touch</button></a>
-        <a
-          href='https://drive.google.com/file/d/1wp1hYBvpPyjSDnxtoHEar_Dnirc-U354/view?usp=sharing'
-          target="_blank"
-          rel="noopener noreferrer">
-          <button className='contact-button'>Résumé</button></a>
-      </section>
-    </main >
-  );
+	return (
+		<main>
+			<NavigationBar />
+			<section className='title-section'>
+				<p className='introduction'>Hello! ¡Holá! Bonjour! My name is</p>
+				<h1>Edwin Del Bosque.</h1>
+				<h1 className='sub-header-title'>I build things for the web.</h1>
+				<p className='description-title'>
+					I'm a <span>front-end software developer</span> based in Denver, CO
+					specializing in building (and occasionally designing) exceptional,
+					high-quality websites and applications.
+				</p>
+				<a href='mailto:edwinbosq@gmail.com'>
+					<button className='contact-button'>Get in touch</button>
+				</a>
+				<a
+					href='https://drive.google.com/file/d/1wp1hYBvpPyjSDnxtoHEar_Dnirc-U354/view?usp=sharing'
+					target='_blank'
+					rel='noopener noreferrer'>
+					<button className='contact-button'>Resume</button>
+				</a>
+			</section>
+		</main>
+	);
 }
 
 export default App;
