86827b94477776ab71838cde34931e5d7f5d9bdf
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-11-28 00:16:56 -0700

    Add media queries for social buttons

--- src/App.js
+++ src/App.js
@@ -6,32 +6,34 @@ import Socials from './Socials';
 
 function App() {
 	return (
-		<main>
-			<NavigationBar />
-			<section className='title-section'>
-				<p className='introduction'>Hello! ¡Hola! Bonjour! My name is</p>
-				<h1>Edwin Del Bosque.</h1>
-				<h1 className='sub-header-title'>I build things for the web.</h1>
-				<p className='description-title'>
-					I'm a <span>front-end software developer</span> based in Denver, CO
-					specializing in building (and occasionally designing) exceptional,
-					high-quality websites and applications.
-				</p>
-				<div>
-					<a href='mailto:edwinbosq@gmail.com'>
-						<button className='contact-button'>Get in touch</button>
-					</a>
-					<a
-						href='https://drive.google.com/file/d/1wp1hYBvpPyjSDnxtoHEar_Dnirc-U354/view?usp=sharing'
-						target='_blank'
-						rel='noopener noreferrer'>
-						<button className='contact-button'>Resume</button>
-					</a>
-				</div>
-			</section>
+		<>
+			<main>
+				<NavigationBar />
+				<section className='title-section'>
+					<p className='introduction'>Hello! ¡Hola! Bonjour! My name is</p>
+					<h1>Edwin Del Bosque.</h1>
+					<h1 className='sub-header-title'>I build things for the web.</h1>
+					<p className='description-title'>
+						I'm a <span>front-end software developer</span> based in Denver, CO
+						specializing in building (and occasionally designing) exceptional,
+						high-quality websites and applications.
+					</p>
+					<div>
+						<a href='mailto:edwinbosq@gmail.com'>
+							<button className='contact-button'>Get in touch</button>
+						</a>
+						<a
+							href='https://drive.google.com/file/d/1wp1hYBvpPyjSDnxtoHEar_Dnirc-U354/view?usp=sharing'
+							target='_blank'
+							rel='noopener noreferrer'>
+							<button className='contact-button'>Resume</button>
+						</a>
+					</div>
+				</section>
+				<AboutMe />
+			</main>
 			<Socials />
-			<AboutMe />
-		</main>
+		</>
 	);
 }
 
