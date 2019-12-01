cd0aa1f048f3cbc27952962680380cc6c6657c26
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-11-30 19:36:49 -0700

    Make Navbar fixed and further fix margins and paddings of Navbar on all resolutions

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -22,59 +22,64 @@ class NavigationBar extends Component {
 
 	render() {
 		return (
-			<nav>
-				<div
-					className={`hamburger-menu ${this.state.isActive &&
-						'hamburger-menu-active'}`}
-					onClick={() => this.toggleMobileMenu()}>
-					<div className='bar-1'></div>
-					<div className='bar-2'></div>
-					<div className='bar-3'></div>
+			<>
+				<div className='nav-wrapper-mobile'></div>
+				<div className='nav-wrapper'>
+					<nav>
+						<div
+							className={`hamburger-menu ${this.state.isActive &&
+								'hamburger-menu-active'}`}
+							onClick={() => this.toggleMobileMenu()}>
+							<div className='bar-1'></div>
+							<div className='bar-2'></div>
+							<div className='bar-3'></div>
+						</div>
+						<div className='logo'>
+							<p>EDWIN DEL BOSQUE</p>
+						</div>
+						<ul
+							className={`navigation-ul ${this.state.isActive &&
+								'navigation-ul-active'}`}>
+							<Link
+								activeClass='active'
+								to='Home'
+								spy={true}
+								smooth={true}
+								offset={-30}
+								duration={1200}>
+								<li onClick={() => this.closeMobileMenu()}>Home</li>
+							</Link>
+							<Link
+								activeClass='active'
+								to='AboutMe'
+								spy={true}
+								smooth={true}
+								offset={60}
+								duration={1200}>
+								<li onClick={() => this.closeMobileMenu()}>About</li>
+							</Link>
+							<Link
+								activeClass='active'
+								to='Projects'
+								spy={true}
+								smooth={true}
+								offset={-150}
+								duration={1200}>
+								<li onClick={() => this.closeMobileMenu()}>Projects</li>
+							</Link>
+							<Link
+								activeClass='active'
+								to='Footer'
+								spy={true}
+								smooth={true}
+								offset={-600}
+								duration={1200}>
+								<li onClick={() => this.closeMobileMenu()}>Contact</li>
+							</Link>
+						</ul>
+					</nav>
 				</div>
-				<div className='logo'>
-					<p>EDWIN DEL BOSQUE</p>
-				</div>
-				<ul
-					className={`navigation-ul ${this.state.isActive &&
-						'navigation-ul-active'}`}>
-					<Link
-						activeClass='active'
-						to='Home'
-						spy={true}
-						smooth={true}
-						offset={-30}
-						duration={1200}>
-						<li onClick={() => this.closeMobileMenu()}>Home</li>
-					</Link>
-					<Link
-						activeClass='active'
-						to='AboutMe'
-						spy={true}
-						smooth={true}
-						offset={60}
-						duration={1200}>
-						<li onClick={() => this.closeMobileMenu()}>About</li>
-					</Link>
-					<Link
-						activeClass='active'
-						to='Projects'
-						spy={true}
-						smooth={true}
-						offset={-150}
-						duration={1200}>
-						<li onClick={() => this.closeMobileMenu()}>Projects</li>
-					</Link>
-					<Link
-						activeClass='active'
-						to='Footer'
-						spy={true}
-						smooth={true}
-						offset={-600}
-						duration={1200}>
-						<li onClick={() => this.closeMobileMenu()}>Contact</li>
-					</Link>
-				</ul>
-			</nav>
+			</>
 		);
 	}
 }
