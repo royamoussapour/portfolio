5f74ebe770ba20e0f98aaaf549bbbb369b18d4a3
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-11-30 22:05:29 -0700

    Make hide/show on scroll navigation bar

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './NavigationBar.scss';
 import { Link, animateScroll as scroll } from 'react-scroll';
+import Headroom from 'react-headroom';
 
 class NavigationBar extends Component {
 	constructor() {
@@ -25,63 +26,65 @@ class NavigationBar extends Component {
 			<>
 				<div className='nav-wrapper-mobile'></div>
 				<div className='nav-wrapper'>
-					<nav>
-						<div
-							className={`hamburger-menu ${this.state.isActive &&
-								'hamburger-menu-active'}`}
-							onClick={() => this.toggleMobileMenu()}>
-							<div className='bar-1'></div>
-							<div className='bar-2'></div>
-							<div className='bar-3'></div>
-						</div>
-						<div className='logo'>
-							<p>EDWIN DEL BOSQUE</p>
-						</div>
-						<ul
-							className={`navigation-ul ${this.state.isActive &&
-								'navigation-ul-active'}`}>
-							<Link
-								activeClass='active'
-								to='Home'
-								spy={true}
-								smooth={true}
-								offset={-30}
-								ignoreCancelEvents={true}
-								duration={1200}>
-								<li onClick={() => this.closeMobileMenu()}>Home</li>
-							</Link>
-							<Link
-								activeClass='active'
-								to='AboutMe'
-								spy={true}
-								smooth={true}
-								offset={60}
-								ignoreCancelEvents={true}
-								duration={1200}>
-								<li onClick={() => this.closeMobileMenu()}>About</li>
-							</Link>
-							<Link
-								activeClass='active'
-								to='Projects'
-								spy={true}
-								smooth={true}
-								offset={-220}
-								ignoreCancelEvents={true}
-								duration={1200}>
-								<li onClick={() => this.closeMobileMenu()}>Projects</li>
-							</Link>
-							<Link
-								activeClass='active'
-								to='Footer'
-								spy={true}
-								smooth={true}
-								offset={-600}
-								ignoreCancelEvents={true}
-								duration={1200}>
-								<li onClick={() => this.closeMobileMenu()}>Contact</li>
-							</Link>
-						</ul>
-					</nav>
+					<Headroom disableInlineStyles>
+						<nav>
+							<div
+								className={`hamburger-menu ${this.state.isActive &&
+									'hamburger-menu-active'}`}
+								onClick={() => this.toggleMobileMenu()}>
+								<div className='bar-1'></div>
+								<div className='bar-2'></div>
+								<div className='bar-3'></div>
+							</div>
+							<div className='logo'>
+								<p>EDWIN DEL BOSQUE</p>
+							</div>
+							<ul
+								className={`navigation-ul ${this.state.isActive &&
+									'navigation-ul-active'}`}>
+								<Link
+									activeClass='active'
+									to='Home'
+									spy={true}
+									smooth={true}
+									offset={-30}
+									ignoreCancelEvents={true}
+									duration={1200}>
+									<li onClick={() => this.closeMobileMenu()}>Home</li>
+								</Link>
+								<Link
+									activeClass='active'
+									to='AboutMe'
+									spy={true}
+									smooth={true}
+									offset={60}
+									ignoreCancelEvents={true}
+									duration={1200}>
+									<li onClick={() => this.closeMobileMenu()}>About</li>
+								</Link>
+								<Link
+									activeClass='active'
+									to='Projects'
+									spy={true}
+									smooth={true}
+									offset={0}
+									ignoreCancelEvents={true}
+									duration={1200}>
+									<li onClick={() => this.closeMobileMenu()}>Projects</li>
+								</Link>
+								<Link
+									activeClass='active'
+									to='Footer'
+									spy={true}
+									smooth={true}
+									offset={-600}
+									ignoreCancelEvents={true}
+									duration={1200}>
+									<li onClick={() => this.closeMobileMenu()}>Contact</li>
+								</Link>
+							</ul>
+						</nav>
+					</Headroom>
 				</div>
 			</>
 		);
