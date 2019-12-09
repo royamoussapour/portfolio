8103dd3cfc0a29286f23b4097761426c21ab8bdf
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-12-08 17:53:05 -0700

    Transform Nav class component into functional react hook

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,121 +1,90 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './NavigationBar.scss';
 import Fade from 'react-reveal/Fade';
 import { Link } from 'react-scroll';
 import Headroom from 'react-headroom';
 
-class NavigationBar extends Component {
-	constructor() {
-		super();
-		this.state = {
-			isActive: false,
-			isTop: true
-		};
-	}
+const NavigationBar = () => {
+	const [isActive, handleIsActive] = useState(false);
 
-	componentDidMount = () => {
-		window.addEventListener('scroll', this.handleScroll, { passive: true });
-	};
-
-	handleScroll = () => {
-		this.setState({ isActive: false });
-		window.pageYOffset >= 70
-			? this.setState({ isTop: false })
-			: this.setState({ isTop: true });
-	};
-
-	toggleMobileMenu = () => {
-		this.setState(prevState => ({
-			isActive: !prevState.isActive
-		}));
-	};
-
-	closeMobileMenu = () => {
-		this.setState({ isActive: false });
-	};
-
-	render() {
-		const { isActive, isTop } = this.state;
-
-		return (
-			<>
-				<div
-					className={`blur-hide ${isActive && 'blur-show'}`}
-					onClick={() => this.closeMobileMenu()}></div>
-				<div className={`nav-wrapper-mobile ${isTop && 'navTop'}`}></div>
-				<div className='nav-wrapper'>
-					<Headroom disableInlineStyles>
-						<Fade>
-							<nav>
-								<div
-									className={`hamburger-menu ${isActive &&
-										'hamburger-menu-active'}`}
-									onClick={() => this.toggleMobileMenu()}>
-									<div className='bar-1'></div>
-									<div className='bar-2'></div>
-									<div className='bar-3'></div>
-								</div>
-								<div className='logo'>
-									<p>EDWIN DEL BOSQUE</p>
-								</div>
-								<Fade top cascade delay={300}>
-									<ul
-										className={`navigation-ul ${isActive &&
-											'navigation-ul-active'}`}>
-										<Link
-											activeClass='active-scroll'
-											to='AboutMe'
-											spy={true}
-											smooth={true}
-											offset={90}
-											ignoreCancelEvents={true}
-											duration={1200}>
-											<li onClick={() => this.closeMobileMenu()}>
-												<span>01.</span>About
-											</li>
-										</Link>
-										<Link
-											to='Projects'
-											spy={true}
-											smooth={true}
-											offset={0}
-											ignoreCancelEvents={true}
-											duration={1200}>
-											<li onClick={() => this.closeMobileMenu()}>
-												<span>02.</span>Projects
-											</li>
-										</Link>
-										<Link
-											to='Career'
-											spy={true}
-											smooth={true}
-											offset={0}
-											ignoreCancelEvents={true}
-											duration={1200}>
-											<li onClick={() => this.closeMobileMenu()}>
-												<span>03.</span>Career
-											</li>
-										</Link>
-										<Link
-											to='Footer'
-											spy={true}
-											smooth={true}
-											offset={-600}
-											ignoreCancelEvents={true}
-											duration={1200}>
-											<li onClick={() => this.closeMobileMenu()}>
-												<span>04.</span>Contact
-											</li>
-										</Link>
-									</ul>
-								</Fade>
-							</nav>
-						</Fade>
-					</Headroom>
-				</div>
-			</>
-		);
-	}
-}
+	return (
+		<>
+			<div
+				className={`blur-hide ${isActive && 'blur-show'}`}
+				onClick={() => handleIsActive(false)}></div>
+			<div className={`nav-wrapper-mobile`}></div>
+			<div className='nav-wrapper'>
+				<Headroom disableInlineStyles>
+					<Fade>
+						<nav>
+							<div
+								className={`hamburger-menu ${isActive &&
+									'hamburger-menu-active'}`}
+								onClick={() => handleIsActive(!isActive)}>
+								<div className='bar-1'></div>
+								<div className='bar-2'></div>
+								<div className='bar-3'></div>
+							</div>
+							<div className='logo'>
+								<p>EDWIN DEL BOSQUE</p>
+							</div>
+							<Fade top cascade delay={300}>
+								<ul
+									className={`navigation-ul ${isActive &&
+										'navigation-ul-active'}`}>
+									<Link
+										activeClass='active-scroll'
+										to='AboutMe'
+										spy={true}
+										smooth={true}
+										offset={90}
+										ignoreCancelEvents={true}
+										duration={1200}>
+										<li onClick={() => handleIsActive(false)}>
+											<span>01.</span>About
+										</li>
+									</Link>
+									<Link
+										to='Projects'
+										spy={true}
+										smooth={true}
+										offset={0}
+										ignoreCancelEvents={true}
+										duration={1200}>
+										<li onClick={() => handleIsActive(false)}>
+											<span>02.</span>Projects
+										</li>
+									</Link>
+									<Link
+										to='Career'
+										spy={true}
+										smooth={true}
+										offset={0}
+										ignoreCancelEvents={true}
+										duration={1200}>
+										<li onClick={() => handleIsActive(false)}>
+											<span>03.</span>Career
+										</li>
+									</Link>
+									<Link
+										to='Footer'
+										spy={true}
+										smooth={true}
+										offset={-600}
+										ignoreCancelEvents={true}
+										duration={1200}>
+										<li onClick={() => handleIsActive(false)}>
+											<span>04.</span>Contact
+										</li>
+									</Link>
+								</ul>
+							</Fade>
+						</nav>
+					</Fade>
+				</Headroom>
+			</div>
+		</>
+	);
+};
 
 export default NavigationBar;
