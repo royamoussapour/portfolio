ad7e1ea519d59d9a056dc8ad1a514184fa543916
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-12-13 16:49:11 -0700

    Add subheaders to projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -8,20 +8,20 @@ import picasso from '../assets/picasso.jpg';
 const projects = [
 	{
 		id: 1,
-		title: 'Picasso | Smart Color Picker (in development)',
+		title: 'Picasso | Smart Color Picker (Beta)',
 		description:
 			'A full-stack web app color picker with back-end and front-end built from the ground up in collaboration with Matt Malone (Full-Stack Dev). Heavy RESTful API development design. 90% testing coverage on the back-end. Create an account or login to create and save your smartly fetched color palettes that actually look good!',
 		repoLink: 'https://github.com/edwindelbosque/picasso-fe',
 		deployLink: 'http://colorsbypicasso.herokuapp.com',
 		image: picasso,
 		role: 'Full-Stack Engineer / UI Designer',
-		weekDuration: 'In development - 2',
+		weekDuration: 'Ongoing 2',
 		technologies1: ['React', 'Hooks', 'Knex', 'Travis CI'],
 		technologies2: ['Node.js', 'Express', 'PostgreSQL', 'Jest']
 	},
 	{
 		id: 2,
-		title: 'The Screening Room',
+		title: 'The Screening Room | Movie Hub',
 		description:
 			'A simplified movie hub. Search for movies or discover new ones. Watch trailers, ratings, summaries, and more. If you like a movie create an account or login to add it to your watchlist and come back to them wherever you are, on any device.',
 		repoLink: 'https://github.com/edwindelbosque/the-screening-room',
@@ -34,7 +34,7 @@ const projects = [
 	},
 	{
 		id: 3,
-		title: 'Artworked',
+		title: 'Artworked | iTunes Artwork Finder',
 		description:
 			'I built this app to solve my own personal problem finding good quality artworks for my music library. Artworked allows you to search and download all iTunes artworks in their original quality. As a last minute extra, it also plays previews.',
 		repoLink: 'https://github.com/edwindelbosque/artworked',
@@ -47,7 +47,7 @@ const projects = [
 	},
 	{
 		id: 4,
-		title: 'The Overlook Management',
+		title: 'The Overlook | Hotel Management',
 		description:
 			'A web app built to help hotel staff manage customers, bookings, bills, and room services. It was this project that I developed a passion for heavy data manipulation.',
 		repoLink: 'https://github.com/edwindelbosque/TheOverlook',
@@ -60,7 +60,7 @@ const projects = [
 	},
 	{
 		id: 5,
-		title: 'Cowboys VS Aliens',
+		title: 'Cowboys VS Aliens | Web Game',
 		description:
 			'A game based on the TV game show Family Feud, but with an Aliens vs Cowboys twist. This was done in collaboration with another three developers. We also built it implementing OOP structured classes in jQuery.',
 		repoLink: 'https://github.com/edwindelbosque/Cowboys-vs-Aliens',
@@ -73,7 +73,7 @@ const projects = [
 	},
 	{
 		id: 6,
-		title: 'Noted.',
+		title: 'Noted. | To-Do List App',
 		description:
 			'A vanilla JS to-do list web app. Create to-do lists and mark them as important so you can then filter them and search for to-do lists. The to-do lists are saved via localStorage. Optimized for all devices.',
 		repoLink: 'https://github.com/edwindelbosque/Noted',
