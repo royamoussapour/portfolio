95b4f0dd5c572a1335aece8584f5614b9001c4dc
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-12-15 15:17:04 -0700

    Compartmentalize App into two different components (Home)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.scss';
-import Fade from 'react-reveal/Fade';
 import NavigationBar from '../NavigationBar/NavigationBar';
 import AboutMe from '../AboutMe/AboutMe';
 import Socials from '../Socials/Socials';
@@ -8,6 +7,7 @@ import Footer from '../Footer/Footer';
 import Projects from '../Projects/Projects';
 import Contact from '../Contact/Contact';
 import Work from '../Work/Work';
+import Home from '../Home/Home';
 import jobs from '../../data/jobs';
 
 const App = () => {
@@ -15,35 +15,7 @@ const App = () => {
 		<>
 			<main name='Home'>
 				<NavigationBar />
-				<Fade cascade delay={600}>
-					<section className='title-section'>
-						<p className='introduction'>Hello! ¡Hola! My name is</p>
-						<h1>Edwin Del Bosque.</h1>
-						<h1 className='sub-header-title'>Building things for the web.</h1>
-						<p className='description-title'>
-							I'm a{' '}
-							<span>
-								<b>Front-End Engineer</b>
-							</span>{' '}
-							based in Denver, CO specialized in building (and occasionally
-							designing), high-quality websites and applications.
-						</p>
-						<div>
-							<Fade bottom cascade delay={800}>
-								<a href='mailto:edwinbosq@gmail.com' className='button-link'>
-									<button className='contact-button'>Get In Touch</button>
-								</a>
-								<a
-									href='https://drive.google.com/file/d/1vEM8J6iY6GSkeWwI75u4RKJkmavw-XmO/view?usp=sharing'
-									target='_blank'
-									rel='noopener noreferrer'
-									className='button-link'>
-									<button className='contact-button'>Resume</button>
-								</a>
-							</Fade>
-						</div>
-					</section>
-				</Fade>
+				<Home />
 				<div className='orb'></div>
 			</main>
 			<AboutMe />
