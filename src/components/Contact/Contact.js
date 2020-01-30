278367b31de06166f04171e7d5746a5be6d37595
Edwin Del Bosque <edwindelbosquer@gmail.com>
2020-01-29 19:11:20 -0700

    Disable message button if no input is anabled

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -14,26 +14,6 @@ const Contact = () => {
 					I'm open and actively looking for job opportunities and
 					collaborations.
 				</p>
-				{/* <a href='mailto:edwinbosq@gmail.com' className='email-button'>
-					<svg
-						version='1.1'
-						xmlns='http://www.w3.org/2000/svg'
-						xlink='http://www.w3.org/1999/xlink'
-						x='0px'
-						y='0px'
-						viewBox='0 0 294.4003906 195.4003906'
-						space='preserve'>
-						<g>
-							<path
-								className='st0'
-								d='M0,0v195.4003906h294.4003906V0H0z M282.4003906,183.4003906H12V12h270.4003906V183.4003906z'></path>
-							<polygon
-								className='st0'
-								points='221.0522461,71.7148438 213.1479492,62.6855469 145.7451172,121.6865234 79.902832,62.7304688
-            71.8979492,71.6699219 145.6552734,137.7138672 	'></polygon>
-						</g>
-					</svg>
-				</a> */}
 				<ContactForm />
 			</main>
 		</Fade>
