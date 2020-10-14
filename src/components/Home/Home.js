22b8c64d70633a4f3eae516c27431a38f0098d49
Edwin Del Bosque <edwin_del_bosque@mckinsey.com>
2020-10-14 08:35:07 -0600

    Update resume

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,46 +1,47 @@
-import React from 'react';
-import './Home.scss';
-import Fade from 'react-reveal/Fade';
+import React from "react";
+import "./Home.scss";
+import Fade from "react-reveal/Fade";
 
 const Home = () => {
-	const name = 'Edwin Del Bosque';
-	const resumeUrl =
-		'https://drive.google.com/file/d/12sU1DRchtoAVYqw034NR1Hk29DpGBVtn/view';
+  const name = "Edwin Del Bosque";
+  const resumeUrl =
+    "https://drive.google.com/file/d/1P56wubhjLR9egjqp8GwB7-vAm19wYq64/view?usp=sharing";
 
-	return (
-		<>
-			<Fade cascade delay={600}>
-				<section className='title-section'>
-					<p className='introduction'>Hello! ¡Hola! My name is</p>
-					<h1>{name}.</h1>
-					<h1 className='sub-header-title'>Building things for the web.</h1>
-					<p className='description-title'>
-						I'm a{' '}
-						<span>
-							<b>Front-End Engineer</b>
-						</span>{' '}
-						and UI design enthusiast based in Denver, CO. My areas of expertise
-						lie in JavaScript ES6+, UX/UI Design, and Test-Driven Development.
-					</p>
-					<div>
-						<Fade bottom cascade delay={800}>
-							<a href='mailto:edwinbosq@gmail.com' className='button-link'>
-								<button className='contact-button'>Get In Touch</button>
-							</a>
-							<a
-								href={resumeUrl}
-								target='_blank'
-								rel='noopener noreferrer'
-								className='button-link'>
-								<button className='contact-button'>Resume</button>
-							</a>
-						</Fade>
-					</div>
-				</section>
-			</Fade>
-			<div className='orb'></div>
-		</>
-	);
+  return (
+    <>
+      <Fade cascade delay={600}>
+        <section className="title-section">
+          <p className="introduction">Hello! ¡Hola! My name is</p>
+          <h1>{name}.</h1>
+          <h1 className="sub-header-title">Building things for the web.</h1>
+          <p className="description-title">
+            I'm a{" "}
+            <span>
+              <b>Front-End Engineer</b>
+            </span>{" "}
+            and UI design enthusiast based in Denver, CO. My areas of expertise
+            lie in JavaScript ES6+, UX/UI Design, and Test-Driven Development.
+          </p>
+          <div>
+            <Fade bottom cascade delay={800}>
+              <a href="mailto:edwinbosq@gmail.com" className="button-link">
+                <button className="contact-button">Get In Touch</button>
+              </a>
+              <a
+                href={resumeUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="button-link"
+              >
+                <button className="contact-button">Resume</button>
+              </a>
+            </Fade>
+          </div>
+        </section>
+      </Fade>
+      <div className="orb"></div>
+    </>
+  );
 };
 
 export default Home;
