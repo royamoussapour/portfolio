ba7b652e5e44a5e3fb6e7fdc760f4d3a4b03eab9
Edwin Del Bosque <edwin_del_bosque@mckinsey.com>
2020-10-27 11:56:04 -0600

    Update projects and jobs

--- src/data/jobs.js
+++ src/data/jobs.js
@@ -1,59 +1,61 @@
 const jobs = [
-	{
-		id: 0,
-		title: 'Software Engineering Apprentice',
-		company: 'McKinsey & Company',
-		location: 'Denver, CO',
-		dateRange: 'Feb 2020 - Present',
-		url: 'https://mckinsey.com',
-		description: []
-	},
-	{
-		id: 1,
-		title: 'Front-End Engineering Student',
-		company: 'Turing School',
-		location: 'Denver, CO',
-		dateRange: 'April 2019 - Jan 2020',
-		url: 'https://turing.io',
-		description: [
-			'Collaborated in fast-paced, cross-functional teams for more than 60 hours a week for 9 months building a variety of interesting and meaningful projects.',
-			'Built robust, scalable, and fully-tested applications with mainly front-end tools and some back-end technologies.'
-		]
-	},
-	{
-		id: 2,
-		title: 'Junior FP&A Financial Analyst Intern',
-		company: 'Zayo Group',
-		location: 'Denver, CO',
-		dateRange: 'June 2018 - June 2019',
-		url: 'https://zayo.com',
-		description: [
-			'Presented variance reviews explaining operational and financial performance of the division.',
-			'Developed attention-to-detail and analytical skills by updating financial models on a weekly basis.'
-		]
-	},
-	{
-		id: 3,
-		title: 'Spanish Tutor',
-		company: 'Regis University',
-		location: 'Denver, CO',
-		dateRange: 'Mar 2017 - May 2019',
-		url: 'https://regis.edu',
-		description: [
-			'Coached students with academic support structuring resources relevant to their coursework.'
-		]
-	},
-	{
-		id: 4,
-		title: 'Transfer Pricing Intern',
-		company: 'Deloitte',
-		location: 'Baja, CA',
-		dateRange: 'Jun 2017 - Aug 2017',
-		url: 'https://deloitte.com',
-		description: [
-			'Assisted in the research and development of pinning down market price ranges between subsidized transactions in accordance to the tax regulations of the state of Baja California.'
-		]
-	}
+  {
+    id: 0,
+    title: "Software Engineering Apprentice",
+    company: "McKinsey & Company",
+    location: "Denver, CO",
+    dateRange: "Feb 2020 - Present",
+    url: "https://mckinsey.com",
+    description: [
+      "Work with a vast array of clients from multiple industries, solving unique problems, and exposed to different development languages and tools.",
+    ],
+  },
+  {
+    id: 1,
+    title: "Front-End Engineering Student",
+    company: "Turing School",
+    location: "Denver, CO",
+    dateRange: "April 2019 - Jan 2020",
+    url: "https://turing.io",
+    description: [
+      "Collaborated in fast-paced, cross-functional teams for more than 60 hours a week for 9 months building a variety of interesting and meaningful projects.",
+      "Built robust, scalable, and fully-tested applications with mainly front-end tools and some back-end technologies.",
+    ],
+  },
+  {
+    id: 2,
+    title: "Junior FP&A Financial Analyst Intern",
+    company: "Zayo Group",
+    location: "Denver, CO",
+    dateRange: "June 2018 - June 2019",
+    url: "https://zayo.com",
+    description: [
+      "Presented variance reviews explaining operational and financial performance of the division.",
+      "Developed attention-to-detail and analytical skills by updating financial models on a weekly basis.",
+    ],
+  },
+  {
+    id: 3,
+    title: "Spanish Tutor",
+    company: "Regis University",
+    location: "Denver, CO",
+    dateRange: "Mar 2017 - May 2019",
+    url: "https://regis.edu",
+    description: [
+      "Coached students with academic support structuring resources relevant to their coursework.",
+    ],
+  },
+  {
+    id: 4,
+    title: "Transfer Pricing Intern",
+    company: "Deloitte",
+    location: "Baja, CA",
+    dateRange: "Jun 2017 - Aug 2017",
+    url: "https://deloitte.com",
+    description: [
+      "Assisted in the research and development of pinning down market price ranges between subsidized transactions in accordance to the tax regulations of the state of Baja California.",
+    ],
+  },
 ];
 
 export default jobs;
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,91 +1,91 @@
-import theScreeningRoom from '../assets/theScreeningRoom.jpg';
-import cowboysVsAliens from '../assets/cowboysVsAliens.jpg';
-import artworked from '../assets/artworked.jpg';
-import theOverlook from '../assets/theOverlook.jpg';
-import picasso from '../assets/picasso.jpg';
-import communityConnect from '../assets/communityConnect.jpg';
+import theScreeningRoom from "../assets/theScreeningRoom.jpg";
+import cowboysVsAliens from "../assets/cowboysVsAliens.jpg";
+import artworked from "../assets/artworked.jpg";
+import smbSite from "../assets/smb-site.png";
+import picasso from "../assets/picasso.jpg";
+import communityConnect from "../assets/communityConnect.jpg";
 
 const projects = [
-	{
-		id: 1,
-		title: 'Picasso - Smart Color Palettes (beta)',
-		description:
-			'A full-stack web app color palette picker with back-end and front-end built from the ground up in collaboration with another developer. Heavy RESTful API development design. 90% testing coverage on the back-end. Create an account or login to create and save your smartly fetched color palettes that actually look good!',
-		repoLink: 'https://github.com/edwindelbosque/picasso-fe',
-		deployLink: 'http://colorsbypicasso.herokuapp.com',
-		image: picasso,
-		role: 'Full-Stack Engineer / UI Designer',
-		weekDuration: 'Ongoing 3',
-		technologies1: ['React', 'Hooks', 'Knex', 'Travis CI'],
-		technologies2: ['Node.js', 'Express', 'PostgreSQL', 'Jest']
-	},
-	{
-		id: 2,
-		title: 'The Screening Room - Movie Hub',
-		description:
-			'A simplified movie hub. Search for movies or discover new ones. Watch trailers, ratings, summaries, and more. If you like a movie create an account or login to add it to your watchlist and come back to them wherever you are, on any device.',
-		repoLink: 'https://github.com/edwindelbosque/the-screening-room',
-		deployLink: 'https://the-screening-room.herokuapp.com',
-		image: theScreeningRoom,
-		role: 'Full-Stack Engineer / UI Designer',
-		weekDuration: 3,
-		technologies1: ['React', 'Redux', 'Router', 'Jest & Enzyme'],
-		technologies2: ['Node.js', 'Express', 'PostgreSQL', 'TheMovieDB API']
-	},
-	{
-		id: 3,
-		title: 'Community Connect Colorado',
-		description:
-			'A React Native mobile application where you can find all of the resources that are available to you in the state of Colorado.',
-		repoLink:
-			'https://github.com/Turing-MOD4-Cross-Pollination/front-end-help-yourself',
-		deployLink:
-			'https://github.com/Turing-MOD4-Cross-Pollination/front-end-help-yourself',
-		image: communityConnect,
-		role: 'Front-End Engineer / UI Designer',
-		weekDuration: 3,
-		technologies1: ['React', 'Native', 'Jest', 'Enzyme'],
-		technologies2: ['Expo', 'Sketch', 'Redux']
-	},
-	{
-		id: 4,
-		title: 'Artworked - iTunes Artwork Finder',
-		description:
-			'I built this app to solve my own personal problem finding good quality artworks for my music library. Artworked allows you to search and download all iTunes artworks in their original quality. As a last minute extra, it also plays previews.',
-		repoLink: 'https://github.com/edwindelbosque/artworked',
-		deployLink: 'https://edwindelbosque.github.io/artworked/',
-		image: artworked,
-		role: 'Front-End Engineer / UI Designer',
-		weekDuration: 1,
-		technologies1: ['React', 'Redux', 'Router'],
-		technologies2: ['Sketch', 'iTunes API', 'Jest & Enzyme']
-	},
-	{
-		id: 5,
-		title: 'The Overlook - Hotel Management',
-		description:
-			'A web app built to help hotel staff manage customers, bookings, bills, and room services. It was this project that I developed a passion for heavy data manipulation.',
-		repoLink: 'https://github.com/edwindelbosque/TheOverlook',
-		deployLink: 'https://edwindelbosque.github.io/TheOverlook/',
-		image: theOverlook,
-		role: 'Front-End Engineer',
-		weekDuration: 1,
-		technologies1: ['jQuery', 'SCSS', 'HTML5', 'Webpack'],
-		technologies2: ['Mocha', 'Chai', 'Spies', 'Sketch']
-	},
-	{
-		id: 6,
-		title: 'Cowboys VS Aliens - Web Game',
-		description:
-			'A game based on the TV game show Family Feud, but with an Aliens vs Cowboys twist. This was done in collaboration with another three developers. We also built it implementing OOP structured classes in jQuery.',
-		repoLink: 'https://github.com/edwindelbosque/Cowboys-vs-Aliens',
-		deployLink: 'https://edwindelbosque.github.io/Cowboys-vs-Aliens/',
-		image: cowboysVsAliens,
-		role: 'Front-End Engineer / UI Designer',
-		weekDuration: 2,
-		technologies1: ['jQuery', 'SCSS', 'Mocha', 'Chai'],
-		technologies2: ['Spies', 'API', 'Skecth']
-	}
+  {
+    id: 5,
+    title: "SMB Small-Business COVID-19 Resources",
+    description:
+      "Built a site from scratch that offers city-specific resources and procedures to help small and medium businesses that were impacted by COVID-19.",
+    repoLink: "https://covid-small-business.mckinsey.com/en/overview/",
+    deployLink: "https://covid-small-business.mckinsey.com/en/overview/",
+    image: smbSite,
+    role: "Front-End Engineer",
+    weekDuration: 1,
+    technologies1: ["Gatsby", "SCSS", "Here API"],
+    technologies2: ["LoopBack4", "Data Management"],
+  },
+  {
+    id: 4,
+    title: "Artworked - iTunes Artwork Finder",
+    description:
+      "I built this app to solve my own personal problem finding good quality artworks for my music library. Artworked allows you to search and download all iTunes artworks in their original quality. As a last minute extra, it also plays previews.",
+    repoLink: "https://github.com/edwindelbosque/artworked",
+    deployLink: "https://edwindelbosque.github.io/artworked/",
+    image: artworked,
+    role: "Front-End Engineer / UI Designer",
+    weekDuration: 1,
+    technologies1: ["React", "Redux", "Router"],
+    technologies2: ["Sketch", "iTunes API", "Jest & Enzyme"],
+  },
+  {
+    id: 1,
+    title: "Picasso - Smart Color Palettes (beta)",
+    description:
+      "A full-stack web app color palette picker with back-end and front-end built from the ground up in collaboration with another developer. Heavy RESTful API development design. 90% testing coverage on the back-end. Create an account or login to create and save your smartly fetched color palettes that actually look good!",
+    repoLink: "https://github.com/edwindelbosque/picasso-fe",
+    deployLink: "http://colorsbypicasso.herokuapp.com",
+    image: picasso,
+    role: "Full-Stack Engineer / UI Designer",
+    weekDuration: "Ongoing 3",
+    technologies1: ["React", "Hooks", "Knex", "Travis CI"],
+    technologies2: ["Node.js", "Express", "PostgreSQL", "Jest"],
+  },
+  {
+    id: 2,
+    title: "The Screening Room - Movie Hub",
+    description:
+      "A simplified movie hub. Search for movies or discover new ones. Watch trailers, ratings, summaries, and more. If you like a movie create an account or login to add it to your watchlist and come back to them wherever you are, on any device.",
+    repoLink: "https://github.com/edwindelbosque/the-screening-room",
+    deployLink: "https://the-screening-room.herokuapp.com",
+    image: theScreeningRoom,
+    role: "Full-Stack Engineer / UI Designer",
+    weekDuration: 3,
+    technologies1: ["React", "Redux", "Router", "Jest & Enzyme"],
+    technologies2: ["Node.js", "Express", "PostgreSQL", "TheMovieDB API"],
+  },
+  {
+    id: 3,
+    title: "Community Connect Colorado",
+    description:
+      "A React Native mobile application where you can find all of the resources that are available to you in the state of Colorado.",
+    repoLink:
+      "https://github.com/Turing-MOD4-Cross-Pollination/front-end-help-yourself",
+    deployLink:
+      "https://github.com/Turing-MOD4-Cross-Pollination/front-end-help-yourself",
+    image: communityConnect,
+    role: "Front-End Engineer / UI Designer",
+    weekDuration: 3,
+    technologies1: ["React", "Native", "Jest", "Enzyme"],
+    technologies2: ["Expo", "Sketch", "Redux"],
+  },
+  {
+    id: 6,
+    title: "Cowboys VS Aliens - Web Game",
+    description:
+      "A game based on the TV game show Family Feud, but with an Aliens vs Cowboys twist. This was done in collaboration with another three developers. We also built it implementing OOP structured classes in jQuery.",
+    repoLink: "https://github.com/edwindelbosque/Cowboys-vs-Aliens",
+    deployLink: "https://edwindelbosque.github.io/Cowboys-vs-Aliens/",
+    image: cowboysVsAliens,
+    role: "Front-End Engineer / UI Designer",
+    weekDuration: 2,
+    technologies1: ["jQuery", "SCSS", "Mocha", "Chai"],
+    technologies2: ["Spies", "API", "Skecth"],
+  },
 ];
 
 export default projects;
